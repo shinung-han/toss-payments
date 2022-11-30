266afbdd95408da8fab4c401a2036d72ed39d972
shinung-han <hanshinung@gmail.com>
2022-11-30 17:51:35 +0900

    toss payments 결제 요청

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { loadTossPayments } from '@tosspayments/payment-sdk';
+import uuid from 'react-uuid';
 
 function App() {
+  const handlePayment = () => {
+    loadTossPayments(`${process.env.REACT_APP_TOSS_TEST_CLIENT_KEY}`)
+      .then((tossPayments) => {
+        tossPayments.requestPayment('카드', {
+          amount: 15000,
+          orderId: `${uuid()}`,
+          orderName: '토스 티셔츠 외 2건',
+          customerName: '박토스',
+          successUrl: 'http://localhost:8080/success',
+          failUrl: 'http://localhost:8080/fail',
+        });
+      })
+      .catch(function (error) {
+        if (error.code === 'USER_CANCEL') {
+          // 결제 고객이 결제창을 닫았을 때 에러 처리
+        } else if (error.code === 'INVALID_CARD_COMPANY') {
+          // 유효하지 않은 카드 코드에 대한 에러 처리
+        }
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Toss Payments Test</h1>
+      <button onClick={handlePayment}>결제하기</button>
     </div>
   );
 }
